98219645de1a4affc9588fe2a18821a4eac54b52
Rohit Sharma <srrohitkandpal@gmail.com>
2023-04-19 07:12:15 +0530

    main: moved ahead and made calculator do calculations.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,49 @@
 window.addEventListener("load", (event) => {
-  let blankVal = '';
+  let blankVal = false;
 
   let enteredVal = document.getElementById('calc-entered-val');
   let finalResult = document.getElementById('calc-result');
 
-  const allBtns = document.querySelectorAll('.calc-btn');
+  const allBtns = document.querySelectorAll('.calc-btn-data-id');
 
   Array.from(allBtns).forEach((btn) => {
-    
     btn.addEventListener('click', (e) => {
-      enteredVal.innerHTML += e.target.innerHTML;
-
       const btnDataId = e.target.dataset.id;
 
-      if (btnDataId == '+' || btnDataId == '-' || btnDataId == '*' || btnDataId == '/' || btnDataId == '%'){
-        if (blankVal == ''){
+      if ((btnDataId == '+' || btnDataId == '-' || btnDataId == '*' || btnDataId == '/' || btnDataId == '%')){
+        if (!blankVal){
           return;
         }
+
+        doCalculation(btnDataId);
       }
+
+      if (btnDataId == 'reset') {
+        enteredVal.innerHTML = '0';
+        return;
+      }
+
+      if (btnDataId == 'delete'){
+        enteredVal.innerHTML = enteredVal.innerHTML.slice(0, -1);
+        return;
+      }
+
+      if (btnDataId == '=') {
+        doCalculation(btnDataId);
+      }
+
+      if (enteredVal.innerHTML == '0'){
+        enteredVal.innerHTML = '';
+      }
+
+      blankVal = true;
+      enteredVal.innerHTML += e.target.innerHTML;
     });
   });
+
+  function doCalculation(operator){
+    const finalVal = eval(enteredVal.innerHTML);
+    finalResult.innerHTML = finalVal;
+  }
+
 });
